3733c12f76e0cf67fc2306a6f7f8cdfcd5e0d7ed
SOUMITRO-SAHA <soumitrosahaofficial@gmail.com>
2026-03-20 17:03:40 +0530

    test(contracts): normalize commit template import
    - Reference: https://github.com/pingdotgg/t3code/issues/638

--- packages/contracts/src/commitTemplates.test.ts
+++ packages/contracts/src/commitTemplates.test.ts
@@ -1,8 +1,5 @@
 import { describe, expect, it } from "vitest";
-import {
-  COMMIT_MODES,
-  CUSTOM_COMMIT_TEMPLATES,
-} from "./commitTemplates";
+import { COMMIT_MODES, CUSTOM_COMMIT_TEMPLATES } from "./commitTemplates";
 
 describe("commitTemplates", () => {
   describe("COMMIT_MODES", () => {
